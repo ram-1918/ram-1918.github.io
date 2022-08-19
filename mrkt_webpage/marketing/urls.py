6cea533dd5648a082d870d75ecba21887e8c84c8
ram-1918 <109194855+ram-1918@users.noreply.github.com>
2022-08-18 23:06:06 -0400

    Delete mrkt_webpage directory

--- mrkt_webpage/marketing/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.base, name = 'base'),
-    #path('', views.content, name = 'content'),
-]
--- mrkt_webpage/marketing/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def base(request):
-    return render(request, 'base.html')
-
-def content(request):
-    return render(request, 'content.html')
